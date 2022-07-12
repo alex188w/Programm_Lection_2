f8b891bbcdfb213507ffc885bae3a01c8cb7ff9f
Алексей Шевченко <alex18w@mail.ru>
2022-07-12 21:11:58 +0500

    Массив Цикл Условие

---
+++ Massive_1/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {1, 12, 31, 4, 15, 16, 17, 28, 4};
+
+int n = array.Length; //определяет длину массива
+int find = 4;
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+      Console.WriteLine(index); 
+      break; //прервать 
+    }
+    //index = index + 1;
+    index++;  
+}
+
